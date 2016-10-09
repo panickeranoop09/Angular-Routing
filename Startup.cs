5e0881f1cc9d0685b7f7a3bdc4647a8232251eed
panickeranoop09 <panickeranoop09@gmail.com>
2016-10-09 07:20:12 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularRouting.Startup))]
+namespace AngularRouting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
